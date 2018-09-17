ededf3792c70202f9bbbdf5e1038b631642eac92
VC Science Labs <dalton_escrofani1@my.vcccd.edu>
2018-09-17 13:25:40 -0700

    Turning in

--- include/linearsearch.h
+++ include/linearsearch.h
@@ -1,10 +1,22 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 70342
+* Assignment: Linear Search
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @DaltonEscrofani
+*/
 #include <cstdint>
 
-namespace edu { namespace vcccd { namespace vc { namespace csv15 {
-	template <class T>
+namespace edu {	namespace vcccd {	namespace vc {	namespace csv15 {
+template <class T>
 	int64_t find(const T& value, T array[], size_t size) {
+		if (array == NULL) return -1;
 		for (int i = 0; i < size; i++) {
-			if (value == array[i]) return i;
+			if (value == array[i])	return i;
 		} return -1;
+
 	}
 }}}}
